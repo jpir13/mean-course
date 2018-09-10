3bd793b3e090de3cce033992818b06fc68525211
Jali Pirhonen <32997880+jpir13@users.noreply.github.com>
2018-09-10 13:39:15 +0300

    added github link as a variable & clickable

--- routes/jali.js
+++ routes/jali.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
+var github = "https://github.com/jpir13/mean-course/";
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('My deployment GitHub repository can be found at: https://github.com/jpir13/mean-course/');
+  res.send('My deployment GitHub repository can be found at: ' + github.link("https://github.com/jpir13/mean-course"));
 });
 
 module.exports = router;
