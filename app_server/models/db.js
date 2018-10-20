462ffbccf65d7e7f4f155b778eb62e948bb0a0e7
Jali Pirhonen <32997880+jpir13@users.noreply.github.com>
2018-10-21 00:44:02 +0300

    mongoose to connect to db

---
+++ app_server/models/db.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+const dbURI = 'mongodb://jali:testaaja13@ds237373.mlab.com:37373/romandb';
+
+mongoose.connect(dbURI, { useNewUrlParser: true });
+
+// loggers
+mongoose.connection.on('connected', function(){
+     console.log('Mongoose connected');
+});
+mongoose.connection.on('error', function(err){
+    console.log('Mongoose conn error', err);
+});
+mongoose.connection.on('disconnected', function(){
+    console.log('Mongoose disconnected');
+});
